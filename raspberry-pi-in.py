d321e41e93f63f26916cdd2425ac6791310918b3
Christian Sanchez <76277520+clickclacknvrwhack@users.noreply.github.com>
2024-01-23 21:21:46 -0800

    Update raspberry-pi-in.py

--- raspberry-pi-in.py
+++ raspberry-pi-in.py
@@ -3,6 +3,7 @@ import time
 import threading
 import os
 
+# the code bellow specifies what pins to collect data from
 STACEY_PIN = 17
 BRIAN_PIN = 23
 
@@ -10,7 +11,7 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(STACEY_PIN, GPIO.IN)
 GPIO.setup(BRIAN_PIN, GPIO.IN)
 
-allowed_to_talk = None
+allowed_to_talk = None # when you start the program no one is allowed to talk
 
 def play_sound():
         print('hey, stop talking')
@@ -18,10 +19,10 @@ def play_sound():
 def check_pico_input():
         global allowed_to_talk
         while True:
-                if GPIO.input(STACEY_PIN) and allowed_to_talk != "stacey":
+                if GPIO.input(STACEY_PIN) and allowed_to_talk != "stacey":# if stacey is talking but should not be talking, "!" means "not"
                         play_sound()
                         print('stacey is talking, but shouldnt be talking')
-                elif GPIO.input(BRIAN_PIN) and allowed_to_talk != "brian":
+                elif GPIO.input(BRIAN_PIN) and allowed_to_talk != "brian":# The same logic is used here, brian is giving sound but it's not his turn
                         play_sound()
                         print('brian is talking talking out of turn')
                 time.sleep(0.1)
@@ -31,13 +32,13 @@ def get_user_input():
         global allowed_to_talk 
         while True:
                 user_input = input("Press 1 to allow Stacey to tslk, 2 for Brian or q for quit:")
-                if user_input == "1":
+                if user_input == "1":        #pressing "1" sets it to stacey, she is allowed to talk
                         allowed_to_talk = "stacey"
                         print("Stacey is allowed to talk.")
-                elif user_input == "2":
+                elif user_input == "2":        #pressing "2" sets it to brian, he is allowed to talk
                         allowed_to_talk = "brian"
                         print("Brian is allowed to talk.")
-                elif user_input.lower() =="q":
+                elif user_input.lower() =="q":        #pressing "q" sets it to quit, the program is over
                         print("bye")
                         GPIO.cleanup()
                         os._exit(0)
